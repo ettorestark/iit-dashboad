734e132c0910f71f62602ef4c87d27a7f021efe2
ettorestark <ettoreastark@gmail.com>
2019-10-14 18:49:03 -0500

    Added comments in home view

--- src/views/dashboard/Home.vue
+++ src/views/dashboard/Home.vue
@@ -6,5 +6,75 @@
 				<h3 class="page-title">Visión general</h3>
 			</div>
 		</div>
+      <div class="col-lg-5 col-md-12 col-sm-12 mb-4">
+        <div class="card card-small blog-comments">
+          <div class="card-header border-bottom">
+            <h6 class="m-0">Mensajes</h6>
+          </div>
+          <div class="card-body p-0">
+            <div class="blog-comments__item d-flex p-3">
+              <div class="blog-comments__avatar mr-3">
+                <img src="images/avatars/1.jpg" alt="User avatar" />
+              </div>
+              <div class="blog-comments__content">
+                <div class="blog-comments__meta text-muted">
+                  <a class="text-secondary" href="#">Sergey Brin</a> – Hace 3 días</span>
+                </div>
+                <p class="m-0 my-1 mb-2 text-muted">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam gravida malesuada massa...</p>
+                <div class="blog-comments__actions">
+                  <div class="btn-group btn-group-sm">
+                    <button type="button" class="btn btn-white">
+                      Leer más 
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="blog-comments__item d-flex p-3">
+              <div class="blog-comments__avatar mr-3">
+                <img src="images/avatars/2.jpg" alt="User avatar" />
+              </div>
+              <div class="blog-comments__content">
+                <div class="blog-comments__meta text-muted">
+                  <a class="text-secondary" href="#">Jeff Bezos</a> on <a class="text-secondary" href="#">Hello World!</a> <span class="text-muted">– Hace 4 días</span>
+                </div>
+                <p class="m-0 my-1 mb-2 text-muted">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam gravida malesuada massa...</p>
+                <div class="blog-comments__actions">
+                  <div class="btn-group btn-group-sm">
+                    <button type="button" class="btn btn-white">
+                      Leer más
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="blog-comments__item d-flex p-3">
+              <div class="blog-comments__avatar mr-3">
+                <img src="images/avatars/3.jpg" alt="User avatar" />
+              </div>
+              <div class="blog-comments__content">
+                <div class="blog-comments__meta text-muted">
+                  <a class="text-secondary" href="#">Jack Dorsey</a> on <a class="text-secondary" href="#">Hello World!</a> <span class="text-muted">– Hace 5 días</span>
+                </div>
+                <p class="m-0 my-1 mb-2 text-muted">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam gravida malesuada massa...</p>
+                <div class="blog-comments__actions">
+                  <div class="btn-group btn-group-sm">
+                    <button type="button" class="btn btn-white">
+                      Leer más
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="card-footer border-top">
+            <div class="row">
+              <div class="col text-center view-report">
+                <button type="submit" class="btn btn-white">Ver todos los mensajes</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
 	</div>
 </template>
