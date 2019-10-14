9405a589f4670e31955ce4973f53e842cbd5900f
ettorestark <ettoreastark@gmail.com>
2019-10-14 12:54:55 -0500

    Fixed some problems with sidebar

--- src/components/dashboard/layout/Sidebar.vue
+++ src/components/dashboard/layout/Sidebar.vue
@@ -1,5 +1,5 @@
 <template>
-  <aside class="main-sidebar col-12 col-md-3 col-lg-2 px-0">
+  <aside id="sidebar" class="main-sidebar col-12 col-md-3 col-lg-2 px-0" @click.prevent="hideNavbar">
     <div class="main-navbar">
       <nav class="navbar align-items-stretch navbar-light bg-white flex-md-nowrap border-bottom p-0">
         <a class="navbar-brand w-100 mr-0" href="#" style="line-height: 25px;">
@@ -77,11 +77,17 @@
 <script>
   export default {
     mounted() {
-      document.querySelectorAll('#sidebar .nav-link').forEach(e => {
+      document.querySelectorAll('#sidebar a:not   ([href="#"])').forEach(e => {
         e.addEventListener('click', () => {
           document.getElementById('sidebar').classList.remove('open');
         })
       });
+    },
+
+    methods: {
+      hideNavbar() {
+        document.getElementById('sidebar').classList.remove('open');
+      }
     }
   }
 </script>
