81d7ead6ef75733543d41eabce604c84d80ae65f
ettorestark <ettoreastark@gmail.com>
2019-10-22 17:54:19 -0500

    Changed path in profile view components

---
+++ src/components/dashboard/profile/edit/ProfileInfo.vue
@@ -0,0 +1,74 @@
+<template>
+	<div>
+<div class="form-row mx-4">
+              	<div class="col-lg-4">
+                  <label for="userProfilePicture" class="text-center w-100 mb-4">Foto de perfil</label>
+                  <div class="edit-user-details__avatar m-auto">
+                    <img :src="'/' + this.$store.state.auth.user.photo" alt="User Avatar">
+                    <label class="edit-user-details__avatar__change">
+                      <i class="fas fa-upload text-primary"></i>
+                      <input type="file" id="userProfilePicture" class="d-none">
+                    </label>
+                  </div>
+                  <button class="btn btn-sm btn-white d-table mx-auto mt-4" type="none"><i class="fas fa-upload mr-1"></i> Subir imagen</button>
+                </div>
+                <div class="col-lg-8">
+                  <div class="form-row">
+                    <div class="form-group col-md-6">
+                      <label for="firstName">Nombres</label>
+                      <input type="text" class="form-control" :value="this.$store.state.auth.user.name" readonly>
+                    </div>
+                    <div class="form-group col-md-6">
+                      <label for="lastName">Apellidos</label>
+                      <input type="text" class="form-control" id="lastName" :value="this.$store.state.auth.user.lastname" readonly>
+                    </div>
+                    <div class="form-group col-md-6">
+                      <label for="userLocation">Ubicaciòn</label>
+                      <div class="input-group input-group-seamless">
+                        <div class="input-group-prepend">
+                          <div class="input-group-text">
+                            <i class="fas fa-map-marker"></i>
+                          </div>
+                        </div>
+                        <input type="text" class="form-control" :value="this.$store.state.auth.user.address" readonly>
+                      </div>
+                    </div>
+                    <div class="form-group col-md-6">
+                      <label for="phoneNumber">Celular</label>
+                      <div class="input-group input-group-seamless">
+                        <div class="input-group-prepend">
+                          <div class="input-group-text">
+                            <i class="fas fa-phone"></i>
+                          </div>
+                        </div>
+                        <input type="text" class="form-control" id="phoneNumber" :value="this.$store.state.auth.user.phone">
+                      </div>
+                    </div>
+                    <div class="form-group col-md-12">
+                      <label for="emailAddress">Corre Electrónico</label>
+                      <div class="input-group input-group-seamless">
+                        <div class="input-group-prepend">
+                          <div class="input-group-text">
+                            <i class="fas fa-envelope"></i>
+                          </div>
+                        </div>
+                        <input type="email" class="form-control" id="emailAddress" :value="this.$store.state.auth.user.email" readonly>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+              </div>
+              <div class="form-row mx-4">
+                <div class="form-group col-md-12">
+                  <label for="userBio">Descripción</label>
+                  <textarea style="min-height: 87px;" id="userBio" name="userBio" class="form-control"></textarea>
+                </div>
+              </div>
+              <div class="form-row mx-4">
+                <div class="form-group col-md-12">
+                  <button class="btn btn-primary">Cambiar información</button>
+                </div>
+              </div>
+	</div>
+</template>
--- src/views/dashboard/profile/Edit.vue
+++ src/views/dashboard/profile/Edit.vue
@@ -1,4 +1,4 @@
-<template>
+edit/<template>
 	<div>
 		<div class="page-header row no-gutters py-4">
 			<div class="col-12 col-sm-4 text-center text-sm-left mb-0">
@@ -11,76 +11,7 @@
 				<div class="card card-small edit-user-details mb-4">
           <div class="card-body p-0">
             <div action="#" class="py-4">
-              <div class="form-row mx-4">
-              	<div class="col-lg-4">
-                  <label for="userProfilePicture" class="text-center w-100 mb-4">Foto de perfil</label>
-                  <div class="edit-user-details__avatar m-auto">
-                    <img :src="'/' + this.$store.state.auth.user.photo" alt="User Avatar">
-                    <label class="edit-user-details__avatar__change">
-                      <i class="fas fa-upload text-primary"></i>
-                      <input type="file" id="userProfilePicture" class="d-none">
-                    </label>
-                  </div>
-                  <button class="btn btn-sm btn-white d-table mx-auto mt-4" type="none"><i class="fas fa-upload mr-1"></i> Subir imagen</button>
-                </div>
-                <div class="col-lg-8">
-                  <div class="form-row">
-                    <div class="form-group col-md-6">
-                      <label for="firstName">Nombres</label>
-                      <input type="text" class="form-control" :value="this.$store.state.auth.user.name" readonly>
-                    </div>
-                    <div class="form-group col-md-6">
-                      <label for="lastName">Apellidos</label>
-                      <input type="text" class="form-control" id="lastName" :value="this.$store.state.auth.user.lastname" readonly>
-                    </div>
-                    <div class="form-group col-md-6">
-                      <label for="userLocation">Ubicaciòn</label>
-                      <div class="input-group input-group-seamless">
-                        <div class="input-group-prepend">
-                          <div class="input-group-text">
-                            <i class="fas fa-map-marker"></i>
-                          </div>
-                        </div>
-                        <input type="text" class="form-control" :value="this.$store.state.auth.user.address" readonly>
-                      </div>
-                    </div>
-                    <div class="form-group col-md-6">
-                      <label for="phoneNumber">Celular</label>
-                      <div class="input-group input-group-seamless">
-                        <div class="input-group-prepend">
-                          <div class="input-group-text">
-                            <i class="fas fa-phone"></i>
-                          </div>
-                        </div>
-                        <input type="text" class="form-control" id="phoneNumber" :value="this.$store.state.auth.user.phone">
-                      </div>
-                    </div>
-                    <div class="form-group col-md-12">
-                      <label for="emailAddress">Corre Electrónico</label>
-                      <div class="input-group input-group-seamless">
-                        <div class="input-group-prepend">
-                          <div class="input-group-text">
-                            <i class="fas fa-envelope"></i>
-                          </div>
-                        </div>
-                        <input type="email" class="form-control" id="emailAddress" :value="this.$store.state.auth.user.email" readonly>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-
-              </div>
-              <div class="form-row mx-4">
-                <div class="form-group col-md-12">
-                  <label for="userBio">Descripción</label>
-                  <textarea style="min-height: 87px;" id="userBio" name="userBio" class="form-control"></textarea>
-                </div>
-              </div>
-              <div class="form-row mx-4">
-                <div class="form-group col-md-12">
-                  <button class="btn btn-primary">Cambiar información</button>
-                </div>
-              </div>
+              <profile-info/>
               <hr>
               <social-networks/>
               <hr>
@@ -94,12 +25,14 @@
 </template>
 
 <script>
-  import ChangePassword from '@/components/dashboard/profile/ChangePassword.vue'
-  import SocialNetworks from '@/components/dashboard/profile/SocialNetworks.vue'
+  import ChangePassword from '@/components/dashboard/profile/edit/ChangePassword.vue'
+  import SocialNetworks from '@/components/dashboard/profile/edit/SocialNetworks.vue'
+  import ProfileInfo from '@/components/dashboard/profile/edit/ProfileInfo.vue'
   export default {
     components: {
       ChangePassword,
-      SocialNetworks
+      SocialNetworks,
+      ProfileInfo
     }
   }
 </script>
